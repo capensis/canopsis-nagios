fe3517094c5a28f4aa08bde8b649a4d854c34526
William Pain <wpain@capensis.fr>
2012-06-14 15:03:33 +0200

    Use ltdl

--- test/test.c
+++ test/test.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <dlfcn.h>
+#include <ltdl.h>
 
-#include "test.h"
 #include "nagios.h"
 
 int event_broker_options = -1;
@@ -10,29 +9,36 @@ int event_broker_options = -1;
 int main (void)
 {
 	void *handle;
-    char *error = NULL;
+	int result = 0;
+
+	printf("Init LTDL\n");
+	result = lt_dlinit();
+	if(result)
+		return ERROR;
+	printf(" + Ok\n");
 
 	printf("Load module ...\n");
 	//handle = lt_dlopen("/home/william/Projets/ndoutils-1.5.2/src/ndomod-3x.o");
-	handle = dlopen("../src/neb2amqp.o", RTLD_LAZY);
-    error = dlerror ();
-    if (error != NULL) {
-        printf (" + Error: %s\n", error);
-        return ERROR;
-    }
+	handle = lt_dlopen("/home/wpain/Bureau/gittmp/neb2amqp/src/neb2amqp.o");
 
+	if (!handle) {
+		printf(" + Error: %s\n", lt_dlerror());
+		return ERROR;
+    	}
 	printf(" + Ok\n");
 
 
 	printf("Unload module\n");
-	dlclose(handle);
+	lt_dlclose(handle);
+	if(result)
+		return ERROR;
+	printf(" + Ok\n");
 
-    error = dlerror ();
-    if (error != NULL) {
-        printf (" + Error: %s\n", error);
-        return ERROR;
-    }
-		
+	printf("Deinit LTDL\n");
+	result = lt_dlexit();
+	if(result)
+		return ERROR;
+
 	printf(" + Ok\n");
 
 	printf("Bye Bye\n");
