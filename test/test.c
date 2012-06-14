79acfba2334542de40f3019ad9b90093d91b5c04
Ziirish <sans.benjamin@gmail.com>
2012-06-14 14:57:17 +0200

    using ldl instead of ltdl

--- test/test.c
+++ test/test.c
@@ -1,45 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ltdl.h>
+#include <dlfcn.h>
 
 #include "test.h"
 #include "nagios.h"
 
-extern int event_broker_options;
+int event_broker_options = -1;
 
 int main (void)
 {
 	void *handle;
-	int result = 0;
-
-	printf("Init LTDL\n");
-	result = lt_dlinit();
-	if(result)
-		return ERROR;
-	printf(" + Ok\n");
+    char *error = NULL;
 
 	printf("Load module ...\n");
 	//handle = lt_dlopen("/home/william/Projets/ndoutils-1.5.2/src/ndomod-3x.o");
-	handle = lt_dlopen("/home/wpain/Bureau/gittmp/neb2amqp/src/neb2amqp.o");
+	handle = dlopen("../src/neb2amqp.o", RTLD_LAZY);
+    error = dlerror ();
+    if (error != NULL) {
+        printf (" + Error: %s\n", error);
+        return ERROR;
+    }
 
-	if (!handle) {
-		printf(" + Error: %s\n", lt_dlerror());
-		return ERROR;
-    	}
 	printf(" + Ok\n");
 
 
 	printf("Unload module\n");
-	lt_dlclose(handle);
-	if(result)
-		return ERROR;
-	printf(" + Ok\n");
+	dlclose(handle);
 
-	printf("Deinit LTDL\n");
-	result = lt_dlexit();
-	if(result)
-		return ERROR;
-
+    error = dlerror ();
+    if (error != NULL) {
+        printf (" + Error: %s\n", error);
+        return ERROR;
+    }
+		
 	printf(" + Ok\n");
 
 	printf("Bye Bye\n");
